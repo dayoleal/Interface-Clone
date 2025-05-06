05ff135d386f8775953dd3e9831f6fbb42c17d4d
Dayô Araújo <dayo.araujosz@gmail.com>
2025-05-06 15:13:59 -0300

    Add files via upload

---
+++ Grupo10_VideoAPP/Grupo10_VideoAPP/Grupo10_VideoAPP/Biblioteca.swift
@@ -0,0 +1,20 @@
+//
+//  Biblioteca.swift
+//  Grupo10_VideoAPP
+//
+//  Created by Aluno Mack on 11/04/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct Biblioteca: View {
+    var body: some View {
+        Button("", systemImage: "trash.fill", action: {})
+    }
+}
+
+#Preview {
+    Biblioteca()
+}
+
---
+++ Grupo10_VideoAPP/Grupo10_VideoAPP/Grupo10_VideoAPP/Grupo10_VideoAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grupo10_VideoAPPApp.swift
+//  Grupo10_VideoAPP
+//
+//  Created by Aluno Mack on 11/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Grupo10_VideoAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
